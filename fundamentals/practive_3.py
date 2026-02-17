18d6f6c9b35afa33f6ef12e4d5d1f80dc5ad2312
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-17 21:13:46 +0530

    Update practive_3.py

--- fundamentals/practive_3.py
+++ fundamentals/practive_3.py
@@ -179,3 +179,49 @@ def sum_of_digits(number):
 number = 12345
 result = sum_of_digits(number)
 print(result)  # Output: 15
+
+
+# Program to display calendar of the given month and year
+import calendar
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+print(calendar.month(yy, mm))
+# output
+#    November 2014
+# Mo Tu We Th Fr Sa Su
+#              1  2
+# 3  4  5  6 7  8  9
+# 10 11 12 13 14 15 16
+# 17 18 19 20 21 22 23
+# 24 25 26 27 28 29 30
+
+# Python Program to Access Index of a List Using for Loop
+l = [21, 44, 35, 11]
+for index, val in enumerate(l):
+    print(index, val)
+#     # Output
+# 0 21
+# 1 44
+# 2 35
+# 3 11
+
+# Example 2: Start the indexing with non zero value
+l  = [21, 44, 35, 11]
+for index, val in enumerate(l, start = 1):
+    print(index, val)
+# Output
+# 1 21
+# 2 44
+# 3 35
+# 4 11
+
+# Example 3: Without using enumerate()
+l = [21, 44, 35, 11]
+for index in range(len(l)):
+    value = l[index]
+    print(index, value)
+# Output
+# 0 21
+# 1 44
+# 2 35
+# 3 11
