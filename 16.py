4d316d63990a68d0a01d200d483ce96444cb65b2
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-20 12:01:53 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,34 +1,3 @@
-# Factorial of a number using recursion
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
-num = 7
-# check if the number is negative
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   print("The factorial of", num, "is", recur_factorial(num))
-# The factorial of 7 is 5040
-
-# challenge:
-def factorial(n):
-    if n < 0:
-        return "Factorial is not defined for negative numbers."
-    elif n == 0 or n == 1:
-        return 1
-    else:
-        result = 1
-        for i in range(2, n + 1):
-            result *= i
-        return result
-# Example usage:
-number = 5
-print(f"The factorial of {number} is {factorial(number)}.")
-
 
 # Python Program to Find Hash of File
 import hashlib
