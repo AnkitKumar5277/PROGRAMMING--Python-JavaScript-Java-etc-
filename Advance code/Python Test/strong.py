071a0ab36b81aa49f76bea384fc45bd59097e37b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-28 11:36:02 +0530

    Update and rename 01 sep, end.py to strong.py

--- Advance code/Python Test/strong.py
+++ Advance code/Python Test/strong.py
@@ -179,7 +179,22 @@ r1 = max(1, 2, 3, 4, 6)
 r2 = max(1, 2, 3)
 r2 = max(2, 3)
 
+def outer_function():
+    var1 = 30 # local
 
+    def inner_function():
+        var2 = 90
+        print(var1)
+
+    def inner_function2():
+        print(var1)
+
+    inner_function()
+    inner_function2()
+    # print(var2)
+
+outer_function()
+# inner_function()
 
 
 
