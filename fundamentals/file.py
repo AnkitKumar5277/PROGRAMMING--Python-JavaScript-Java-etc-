999ac171f519fb8afda8d50260df7ad57ec12659
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-03-12 17:44:57 +0530

    Create file.py

---
+++ fundamentals/file.py
@@ -0,0 +1,9 @@
+# program_to_count_the_number_of_capital_letter.py
+with open("SOME_LARGE_FILE.txt","r") as file:
+    count = 0
+    text = file.read()
+    for character in text:
+        if character.isupper():
+            count +=1
+
+    print("Number of uppercase letters:", count)
