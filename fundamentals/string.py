f0021056df731e8432038fd0bcc1f743c9fc2852
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-03-12 20:27:55 +0530

    Update string.py

--- fundamentals/string.py
+++ fundamentals/string.py
@@ -15,3 +15,11 @@ name = "Charlie"
 age = 35
 print(f"My name is {name} and I'm {age} years old.") 
 # Output: My name is Charlie and I'm 35 years old.
+
+# Create a list
+my_list = ["apple", "banana", "orange", "grape"]
+# Accessing elements using index
+print("First element:", my_list[0])  # Output: apple
+print("Last element:", my_list[-1])   # Output: grape (negative index for last element)
+# Accessing a range of elements (slicing)
+print("First two elements:", my_list[0:2])  # Output: ['apple', 'banana']
