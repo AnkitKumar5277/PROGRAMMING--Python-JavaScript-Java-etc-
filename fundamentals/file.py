d3e3d047ed98c9997b42d82b25af09915416647e
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-03-19 00:07:37 +0530

    Update file.py

--- fundamentals/file.py
+++ fundamentals/file.py
@@ -7,3 +7,17 @@ with open("SOME_LARGE_FILE.txt","r") as file:
             count +=1
 
     print("Number of uppercase letters:", count)
+
+
+# pickling_unpickling
+import pickle
+# Sample data (a dictionary)
+data = {"name": "Alice", "age": 25, "city": "New York"}
+# Pickling: Saving the object to a file
+with open("data.pkl", "wb") as file:  # 'wb' means write binary
+    pickle.dump(data, file)
+    print("Data pickled successfully!")
+# Unpickling: Loading the object from the file
+with open("data.pkl", "rb") as file:  # 'rb' means read binary
+    loaded_data = pickle.load(file)
+    print("Unpickled data:", loaded_data)
