76cb506810448dd71442eb623c0f67e273a9332b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-06 20:56:12 +0530

    Update variables.py

--- fundamentals/variables.py
+++ fundamentals/variables.py
@@ -8,9 +8,47 @@ g = {'name': 'John', 'age': 20}  # dict
 h = True        # boolean
 i = None        # none
 
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
+
+kilometers = float(input("Enter value in kilometers: "))
+conv_fac = 0.621371
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+
+celsius = float(input("Enter: "))
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+
+
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
 a = 10
 b = 10
 print(a is b)  # Output: True (because small integers share the same memory in Python)
+
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+temp = x
+x = y
+y = temp
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
+x = 5
+y = 10
+x, y = y, x
+print("x =", x)
+print("y =", y)
+
+
 
 name = "Hello"
 print("H" in name)  # Output: True
@@ -19,6 +57,21 @@ print("H" in name)  # Output: True
 my_name = "John"
 _age = 20
 marks1 = 88
+
+#Addition and Subtraction
+x = x + y
+y = x - y
+x = x - y
+#Multiplication and Division
+x = x * y
+y = x / y
+x = x / y
+XOR swap
+
+#This algorithm works for integers only
+x = x ^ y
+y = x ^ y
+x = x ^ y
 
 # Find square root of real or complex numbers
 import cmath
