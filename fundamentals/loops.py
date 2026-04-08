5140854299d28544a25d8021af88d408f43f6c47
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-08 17:20:01 +0530

    Update loops.py

--- fundamentals/loops.py
+++ fundamentals/loops.py
@@ -1,3 +1,21 @@
+# Python Program to Find LCM
+# Python Program to find the L.C.M. of two input number
+def compute_lcm(x, y):
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+   return lcm
+num1 = 54
+num2 = 24
+print("The L.C.M. is", compute_lcm(num1, num2))
+
 # Program to check if a number is prime or not
 num = int(input("Enter a number: "))
 flag = False
