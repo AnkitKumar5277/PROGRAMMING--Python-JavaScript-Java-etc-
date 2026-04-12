9cdec6f0754e5dc7ec51817cbe00defd359d2469
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-12 20:05:05 +0530

    Update dictionary.py

--- fundamentals/dictionary.py
+++ fundamentals/dictionary.py
@@ -1,3 +1,11 @@
+# Example 3: Using copy() and update()
+d1 = {1: 'a', 2: 'b'}
+d2 = {2: 'c', 4: 'd'}
+d3 = d2.copy()
+d3.update(d1)
+print(d3)
+# {2: 'b', 4: 'd', 1: 'a'}
+
 # Create a dictionary
 my_dict = {"name": "Amit", "age": 20}
 # 1. Add new key-value pair
