905b31a7d7c7afc8cf75f1641057cafdad0f6a4b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-13 13:17:10 +0530

    Update dictionary.py

--- fundamentals/dictionary.py
+++ fundamentals/dictionary.py
@@ -1,3 +1,9 @@
+# Example 2: Using the ** Operator
+d1 = {1: 'a', 2: 'b'}
+d2 = {2: 'c', 4: 'd'}
+print({**d1, **d2})
+# {1: 'a', 2: 'c', 4: 'd'}
+
 # Example 3: Using copy() and update()
 d1 = {1: 'a', 2: 'b'}
 d2 = {2: 'c', 4: 'd'}
