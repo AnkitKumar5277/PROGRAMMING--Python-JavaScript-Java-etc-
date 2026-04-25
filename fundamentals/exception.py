7c90ab37243fa1676a9637aa39e58ab80ca978f4
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-25 20:30:54 +0530

    Update exception.py

--- fundamentals/exception.py
+++ fundamentals/exception.py
@@ -1,3 +1,30 @@
+# Example 2 error of result
+try:
+  a = int(input("Enter a number : "))
+  c = 1 / a
+  print(c)
+
+except Exception as e:
+  print("Exception occured")
+  # print(e) # -> gives result of error
+
+print("Thanks for using this code")
+
+while(True):
+  print("Press q to quit")
+  a = input("Enter a number : ")
+  if a == 'q':
+    break
+  try:
+    print("Tring...")
+    a = int(a)
+    if a > 6:
+      print("You Entered a number greater thann 6")
+  except Exception as e:
+    print(f"Your resulted in an error: {e}")
+
+print("Thanks for playing this game")  
+
 # write a program to display a / b where a and b are integers if b = 0, display infinite by handling the zero division error
 '''
 a = int(input("enter number a : "))
